81fed4feed91b9a5b0b7db1813a1cab85e0be5fe
sawhtoo <sawhtoothithlaing@gmail.com>
2025-10-01 21:21:36 +0900

    community-delete-okay-1Oct2025

--- lib/features/home/widgets/community_constants.dart
+++
@@ -1,9 +0,0 @@
-class CommDims {
-  static const double vPad = 12;
-  static const double headerH = 106;
-  static const double rowH = 56;
-  static const double rowGap = 8;
-  static const int rows = 3;
-  
-  static double cardHeight() => headerH + rows * rowH + (rows - 1) * rowGap + 2 * vPad;
-}
